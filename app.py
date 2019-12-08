fb4ea46101e1a84531e409d147f1c0dc81d43c8c
Thomas <tahjones75@gmail.com>
2019-12-08 19:39:40 +0000

    connected mongodb database 'sequencingMetricsDB to app.py

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
 
+app.config["MONGO_DBNAME"] = 'sequencingMetricsDB'
+app.config["MONGO_URI"] = 'mongodb+srv://seqMetRoot:seqMetR00tUser@sequencingmetricsdb-kpu2s.mongodb.net/sequencingMetricsDB?retryWrites=true&w=majority'
+
+
+mongo = PyMongo(app)
+
 @app.route("/")
-def hello():
-    return "Hello World!!!"
+@app.route("/get_metrics")
+def getMetrics():
+    return render_template("get-metrics.html", metrics=mongo.db.seqMetCol.find())
 
 
 if __name__ == "__main__":
