40d0f5858072102448032daf282b8a5564c78e8f
Thomas <tahjones75@gmail.com>
2019-12-09 17:59:03 +0000

    removed commented out app.run() statement

--- app.py
+++ app.py
@@ -18,5 +18,4 @@ def getMetrics():
 
 
 if __name__ == "__main__":
-    # app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)
     app.run(host=os.environ.get('IP'), port=os.environ.get('PORT'), debug=True)
