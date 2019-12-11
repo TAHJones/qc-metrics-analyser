00d1644423b663b6a807333fc6c0987adc538b69
Thomas <tahjones75@gmail.com>
2019-12-11 21:52:22 +0000

    added new routes & modifed route to index.html

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, flash
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.secret_key = "my_password"
 
 
 app.config["MONGO_DBNAME"] = 'sequencingMetricsDB'
@@ -11,10 +12,46 @@ app.config["MONGO_URI"] = 'mongodb+srv://seqMetRoot:seqMetR00tUser@sequencingmet
 
 mongo = PyMongo(app)
 
+# @app.route('/', methods=["GET", "POST"], metrics=mongo.db.seqMetCol.find())
+@app.route('/', methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        flash("Thanks {}, your data has been entered successfully".format(
+            request.form["name"]))
+
+    return render_template("index.html")
+
+"""
 @app.route("/")
+def index():
+    return render_template("index.html", metrics=mongo.db.seqMetCol.find())
+"""
+
+"""
 @app.route("/get_metrics")
 def getMetrics():
     return render_template("get-metrics.html", metrics=mongo.db.seqMetCol.find())
+"""
+
+
+@app.route('/runs')
+def runs():
+    return render_template("runs.html")
+
+
+@app.route('/chemistry')
+def chemistry():
+    return render_template("chemistry.html")
+
+
+@app.route('/experiments')
+def experiments():
+    return render_template("experiments.html")
+
+
+@app.route('/users')
+def users():
+    return render_template("users.html")
 
 
 if __name__ == "__main__":
