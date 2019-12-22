54558028e0936ca76e567b7a88d8d6b66acc8026
Thomas <tahjones75@gmail.com>
2019-12-22 09:56:25 +0000

    add signup route & fix username route

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for, flash, session
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.secret_key = "my_password"
@@ -36,21 +37,49 @@ def index():
 
     return render_template("index.html", metrics=mongo.db.seqMetCol.find())
 
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    """ Add new user to database """
+    metricsdb = mongo.db.seqMetCol
+    users = metricsdb.find({}, {'user:1', '_id:0'})
+    if request.method == "POST":
+        for user in users:
+            if user == request.form.get("username"):
+                print("username already exists, please enter a unique username")
+            else:
+                newuser = {'user': request.form.get('newUsername')}
+                metricsdb.insert_one(newuser)
+                return redirect(url_for('index'))
+    return render_template("signup.html")
+
+
+
 
 @app.route("/user/<username>", methods=["GET", "POST"])
 def username(username):
     """Add and display chat messages"""
+    metricsdb = mongo.db.seqMetCol
     if request.method == "POST":
-        username = session["username"]
-        # session.clear()
-        # name = request.form["name"]
-        # pool = request.form["pool"]
-        # yields = request.form["yield"]
-        # clusterDensity = request.form["clusterDensity"]
-        # passFilter = request.form["passFilter"]
-        # q30 = request.form["q30"]
-        metrics = mongo.db.seqMetCol
-        metrics.insert_one(request.form.to_dict())
+        # session["username"] = request.form["username"]
+        # username = session["username"]
+        # username = request.form.get("name")
+        # name = username
+        pool = request.form.get("pool")
+        yields = request.form.get("yield")
+        clusterDensity = request.form.get("clusterDensity")
+        passFilter = request.form.get("passFilter")
+        q30 = request.form.get("q30")
+        session.clear()
+        run = {
+            'user': username,
+            'pool': pool,
+            'yield': yields,
+            'clusterDensity': clusterDensity,
+            'passFilter': passFilter,
+            'q30': q30
+            }
+        metricsdb.insert(run)
+        # metrics.insert_one(request.form.to_dict())
         return redirect(url_for("username", username=username))
 
     session.clear()
