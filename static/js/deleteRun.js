6d704eaeadfc645139ec8f89ffd03740c428537a
Thomas <tahjones75@gmail.com>
2020-02-10 17:21:12 +0000

    removed script tags from top & bottom of page

--- static/js/deleteRun.js
+++ static/js/deleteRun.js
@@ -1,27 +1,24 @@
-<script>
-    let pageLocation = JSON.parse({{ pageLocation|tojson }});
-    const deleteRunForm = document.getElementById("deleteRunForm");
-    const deleteRunButton = document.getElementById("deleteRunButton");
-    const deleteRunList = document.getElementById("deleteRunList");
+let pageLocation = JSON.parse({{ pageLocation|tojson }});
+const deleteRunForm = document.getElementById("deleteRunForm");
+const deleteRunButton = document.getElementById("deleteRunButton");
+const deleteRunList = document.getElementById("deleteRunList");
 
-    /**
-    * Function is called by "DOMContentLoaded" event. It hides or displays different sections of the page depending on whether the 'pageLocation' variable passed from the server contains the string 'runDeleted' or 'deleteRunForm'.
-    * @param {string} pageSection - pageLocation variable which can be string "runDeleted" or "deleteRunForm".
-    */
-    function hidePageSection(pageSection){
-        if(pageLocation == "runDeleted"){
-            deleteRunForm.classList.add("hide");
-            deleteRunButton.classList.add("hide");
-            deleteRunList.classList.add("deleted");
-        } else if(pageLocation == "deleteRunForm") {
-            deleteRunForm.classList.remove("hide");
-            deleteRunButton.classList.remove("hide");
-            deleteRunList.classList.remove("deleted");
-        }
+/**
+* Function is called by "DOMContentLoaded" event. It hides or displays different sections of the page depending on whether the 'pageLocation' variable passed from the server contains the string 'runDeleted' or 'deleteRunForm'.
+* @param {string} pageSection - pageLocation variable which can be string "runDeleted" or "deleteRunForm".
+*/
+function hidePageSection(pageSection){
+    if(pageLocation == "runDeleted"){
+        deleteRunForm.classList.add("hide");
+        deleteRunButton.classList.add("hide");
+        deleteRunList.classList.add("deleted");
+    } else if(pageLocation == "deleteRunForm") {
+        deleteRunForm.classList.remove("hide");
+        deleteRunButton.classList.remove("hide");
+        deleteRunList.classList.remove("deleted");
     }
+}
 
-    document.addEventListener("DOMContentLoaded", function() {
-        hidePageSection(pageLocation);
-    });
-
-</script>
+document.addEventListener("DOMContentLoaded", function() {
+    hidePageSection(pageLocation);
+});
