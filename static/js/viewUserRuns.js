8eb0e921314645a1303e07d6ad04f47e60f30a0c
Thomas <tahjones75@gmail.com>
2020-02-17 09:09:30 +0000

    removed hide/show controls for buttons & targeted new 'userRunBg' section

--- static/js/viewUserRuns.js
+++ static/js/viewUserRuns.js
@@ -1,21 +1,29 @@
 const userForm = document.getElementById("userFormBg");
 const userRuns = document.getElementById("userRunsBg");
-const returnToForm = document.getElementById("returnToForm");
-const deleteRun = document.getElementById("deleteRun");
-const updateRun = document.getElementById("updateRun");
-const selectFilteredRunForm = document.getElementById("selectFilteredRunForm");
+const userRun = document.getElementById("userRunBg");
+// const returnToForm = document.getElementById("returnToForm");
+// const deleteRun = document.getElementById("deleteRun");
+// const updateRun = document.getElementById("updateRun");
+// const selectFilteredRunForm = document.getElementById("selectFilteredRunForm");
 
 function hidePageSection(pageSection){
     if(pageSection=="userRun"){
-        userRuns.classList.remove("hide");
+        userRun.classList.remove("hide");
+        userRuns.classList.add("hide");
         userForm.classList.add("hide");
-        selectFilteredRunForm.classList.add("hide")
-        deleteRun.classList.remove("invisible");
-        updateRun.classList.remove("invisible");            
+        // selectFilteredRunForm.classList.add("hide")
+        // deleteRun.classList.remove("invisible");
+        // updateRun.classList.remove("invisible");            
     } else if(pageSection=="userRuns") {
+        userRun.classList.add("hide");
         userRuns.classList.remove("hide");
         userForm.classList.add("hide");
-        selectFilteredRunForm.classList.remove("hide")
+        // selectFilteredRunForm.classList.remove("hide")
+    } else if(pageSection=="userForm"){
+        userRun.classList.add("hide");
+        userRuns.classList.add("hide");
+        userForm.classList.remove("hide");
+        // selectFilteredRunForm.classList.remove("hide")
     }
 }
 
