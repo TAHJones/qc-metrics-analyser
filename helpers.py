edf23892435a3fafc9606baba3b3959e5ad0494f
Thomas <tahjones75@gmail.com>
2020-03-07 18:14:07 +0000

    added comment to addUserRun helper function

--- helpers.py
+++ helpers.py
@@ -289,40 +289,45 @@ class Helpers:
         passFilter = data["passFilter"]
         q30 = data["q30"]
         if yields < 1 or yields > 250:
-            message = "Yield should be between 1 and 250 but is {}".format(data.yields)
+            message = "Yield should be between 1 and 250, but is {}".format(yields)
             return message
         elif clusterDensity < 50 or clusterDensity > 250:
-            message = "Cluster density should be between 50 and 250 but is {}".format(clusterDensity)
+            message = "Cluster density should be between 50 and 250, but is {}".format(clusterDensity)
             return message
         elif passFilter < 1 or passFilter > 100:
-            message = "Pass filter should be between 1 and 100 but is {}".format(passFilter)
+            message = "Pass filter should be between 1 and 100, but is {}".format(passFilter)
             return message
         elif q30 < 1 or q30 > 100:
-            message = "Q30 should be between 1 and 100 but is {}".format(q30)
+            message = "Q30 should be between 1 and 100, but is {}".format(q30)
             return message
         else:
             message = "pass"
             return message
 
 
+    """ Gets form data for new user run & checks data is correct with help of getRunList & checkMetricValues functions.
+    If data is incorrect returns message var which is passed to flash message function.
+    If data is correct adds new run to database and returns message var which is passed to flash message function.
+    Takes database query & username as parameters """
     @staticmethod
     def addUserRun(database, user):
-        message = None
         runList = Helpers.getRunList(database)
         formData = Helpers.getFilteredFormData("user", "chemistry", "experiment", "comment")
+        message = Helpers.checkMetricValues(formData)
         poolNumber = formData["pool"]
         formName = formData["user"]
         if user != formName:
             message = "Username '{}' is incorrect".format(formName)
+            return message
         elif user == formName:
             for run in runList:
                 if run["pool"] == poolNumber:
                     message = "Pool_{} already exists, enter a unique number".format(poolNumber)
-            if message == None:
+                    return message
+            if message == "pass":
                 database.insert_one(formData)
                 message = "Pool_{} has been successfully added".format(poolNumber)
         return message
-
 
 
     """ Checks if database query returns empty list. If true it replaces empty 
